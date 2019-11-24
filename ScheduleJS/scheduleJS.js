d569e991a075131499a76c0837d336493803e8d9
Marco <marriaga158@gmail.com>
2019-11-24 12:09:28 -0500

    linked jquery and made variables for the checkboxes

---
+++ ScheduleJS/scheduleJS.js
@@ -0,0 +1,5 @@
+var checkbox103 = $("value=CAL103");
+var checkbox123 = $("value=MA123");
+var checkbox115 = $("value=CS115");
+var checkbox146 = $("value=CS146");
+var checkbox111 = $("value=PEP111");
