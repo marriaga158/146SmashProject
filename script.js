c02e7544feb7ef1c5f8f98894c36288e0f2341fc
thekevinliao <kev.revolution@gmail.com>
2019-12-03 22:33:35 -0500

    added color theme

---
+++ script.js
@@ -0,0 +1,31 @@
+function redTheme(elem){
+    if (elem.checked){
+        // alert("Im checked"); test code
+        // this needs to remove the td elements and add one cal103
+        $("body").css("background-color", "red"); // this removes all of the <td> elements that need to be cleared for 103
+    }
+}
+
+function blueTheme(elem){
+    if (elem.checked){
+        // alert("Im checked"); test code
+        // this needs to remove the td elements and add one cal103
+        $("body").css("background-color", "cyan"); // this removes all of the <td> elements that need to be cleared for 103
+    }
+}
+
+function greenTheme(elem){
+    if (elem.checked){
+        // alert("Im checked"); test code
+        // this needs to remove the td elements and add one cal103
+        $("body").css("background-color", "green"); // this removes all of the <td> elements that need to be cleared for 103
+    }
+}
+
+function pinkTheme(elem){
+    if (elem.checked){
+        // alert("Im checked"); test code
+        // this needs to remove the td elements and add one cal103
+        $("body").css("background-color", "pink"); // this removes all of the <td> elements that need to be cleared for 103
+    }
+}
